7d9c5babe87b4c890aee5fd4249aeaf02f2a0f6a
Spencer <spencerlow1@outlook.com>
2022-12-01 00:53:30 -0500

    .gitignore node_modules and table outline

---
+++ node-api-postgres/sql/ddl.sql
@@ -0,0 +1,42 @@
+
+
+/*
+needs reordering, just putting outline here first
+
+create table Book
+	(
+    );
+
+create table Bookstore
+	(
+    );
+
+create table User
+	(
+    );
+
+create table Cart
+	(
+    );
+
+create table Order
+	(
+    );
+
+create table Order_contents
+	(
+    );
+
+create table Publisher
+	(
+    );
+
+create table Has_numbers
+	(
+    );
+
+create table Book_records
+	(
+    );
+
+*/
