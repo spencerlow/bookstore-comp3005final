f46617bc1f1c018f4598315109f675c772ec69bf
Spencer <spencerlow1@outlook.com>
2022-12-01 01:06:41 -0500

    ddl and dml small experimental update of data

--- node-api-postgres/sql/ddl.sql
+++ node-api-postgres/sql/ddl.sql
@@ -1,10 +1,21 @@
 
 
 /*
-needs reordering, just putting outline here first
++needs reordering, just putting outline here first
++the types are temporary here
 
 create table Book
 	(
+	ISBN numberic(5,0) UNIQUE NOT NULL,
+	stockQuantity INT NOT NULL,
+	royalty INT NOT NULL,
+	lastMonthSales INT NOT NULL,
+	page_Num INT NOT NULL,
+	price numberic(10,2) NOT NULL,
+	pID numberic(5,0) NOT NULL,
+    
+	primary key (ISBN),
+	foreign key (pID) references publisher (pID)        
     );
 
 create table Bookstore
---
+++ node-api-postgres/sql/dml.sql
@@ -0,0 +1,11 @@
+/*
+
++ISBN,stockQuantity,royalty,lastMonthSales,page_Num,price,pID
+
+INSERT INTO BOOK
+VALUES      ('0399214577',5,5,3,200,15,3),
+            ('0123859654',4,5,4,300,16,5),
+            ('0805014853',1,5,2,400,19,6);
+
+
+*/
