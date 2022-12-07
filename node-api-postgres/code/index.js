03392d7b359ba2e13a07ca5562626f6af4361de7
Spencer <spencerlow1@outlook.com>
2022-12-06 22:32:25 -0500

    rough searching

--- node-api-postgres/code/index.js
+++ node-api-postgres/code/index.js
@@ -35,6 +35,10 @@ app.get('/users/changeUser=:n', changeUser)
 app.get('/users/addUser',db.addUser)
 //app.get('/users/addUser?shipping=:uShipping&billing=:uBilling',db.addUser)
 
+app.get('/search',search)
+
+app.get('/search?',db.searchQuery)
+
 app.listen(port, () => {
 console.log(`App running on port localhost:${port}.`)
 })
@@ -48,5 +52,11 @@ function changeUser(req, res)
   console.log(req.url.split('=')[1]);
   app.locals.currUID = req.url.split('=')[1];
   res.status(200).redirect("http://localhost:3000/users");
+  return;
+}
+
+function search(req,res)
+{
+  res.status(200).send(pug.renderFile("search.pug",{currUID:app.locals.currUID}));
   return;
 }
--- node-api-postgres/code/search.js
+++ node-api-postgres/code/search.js
@@ -6,8 +6,8 @@ document.getElementById("submit").onclick = search;
 function search(){
     console.log("clicked");
 
-    let newurl = "/seach?"    
+    //let newurl = "/seach?"    
     //console.log(document.getElementById("userSelect").value)
     //window.location.href = window.location.href;
-    + "=" + document.getElementById("userSelect").value;
+    //+ "=" + document.getElementById("userSelect").value;
   }
